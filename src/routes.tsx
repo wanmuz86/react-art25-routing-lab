f9f79a1c47f4d0d5bd0d32eb126313a2fea04a97
Wan Muzaffar <wanmuz86@gmail.com>
2025-08-19 09:50:20 +0700

    adding subpath url and sublayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,9 @@ import About from "./About";
 import AppLayout from "./AppLayout";
 import NotFound from "./NotFound";
 import User from "./User";
+import ProfileLayout from "./profiles/ProfileLayout";
+import Settings from "./profiles/Settings";
+import Details from "./profiles/Details";
 
 // The latest to do in react router v6+
 export const router = createBrowserRouter([
@@ -31,6 +34,22 @@ export const router = createBrowserRouter([
                  // localhost:5173/anything-not-found
                 path:"*",
                 element:<NotFound/>
+            },
+            {
+                path:"profile/:userId",
+                element:<ProfileLayout/>,
+                children:[
+                    {
+                        index:true, element:<div>Select a tab(Settins or Details)</div>
+                    },
+                    {
+                     path:"settings", element:<Settings/>,
+                    
+                    },
+                    {
+                    path:"details", element:<Details/>
+                    }
+                ]
             }
         ]
     }
