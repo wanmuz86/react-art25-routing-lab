e353a63ced78a941512354b1b791737a7368164a
Wan Muzaffar <wanmuz86@gmail.com>
2025-08-19 09:19:09 +0700

    add useNavigate and parameters with ID

--- src/About.tsx
+++ src/About.tsx
@@ -3,11 +3,15 @@ import { useNavigate } from 'react-router-dom'
 
 const About:React.FC = () => {
 
+  // Retrieving the navigate function from a hook
+  // useNavigate is a React Router hook that returns a function for navigation.
+  // General statement about hooks: Some hooks (like useState, useReducer) return arrays/objects containing multiple values. 
+  // But useNavigate specifically returns just a single function.
   const navigate = useNavigate();
 
   const goToUser = (userId:string) => {
     // eg scenario: after API called, after order purchase, after payment, redirect to success or to failure page
-    navigate(`/user/${userId}`)
+   navigate(`/user/${userId}`)
   }
 
   return (
